d8396b7024662350c3e8d57b81806ec6a7ec7eb5
Ali <akbarsatriaxtkj2@gmail.com>
2024-09-24 16:13:05 +0700

    feat: change implementation of fetch and rendering lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-import Table from "./Table";
-import "./App.css";
 import { useState, useEffect } from "react";
+import Table from "./Table";
+import Error from "./Error";
+import "./App.css";
 
 function App() {
+  const [error, setError] = useState(false);
+  const [data, setData] = useState({});
   const apiKey = import.meta.env.VITE_CURRENCY_API_KEY;
   const url = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${apiKey}&symbols=CAD,IDR,JPY,CHF,EUR,USD`;
-  // const test = {
-  //   test: "test",
-  // };
 
-  const [data, setData] = useState({});
+  async function handleFetch(url) {
+    try {
+      const res = await fetch(url);
+
+      if (res.status != 200) {
+        setError(true);
+        return;
+      }
+
+      const data = await res.json();
+      const { rates } = data;
+      setData(rates);
+    } catch (err) {
+      console.error(err);
+      setError(true);
+    }
+  }
+
   useEffect(() => {
-    fetch(url)
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        const { rates } = resData;
-        setData(rates);
-      });
+    handleFetch(url);
   }, []);
+
+  if (error) {
+    return <Error />;
+  }
 
   return (
     <div id="container">
--- src/Table.jsx
+++ src/Table.jsx
@@ -1,71 +1,39 @@
 import "./Table.css";
 
 export default function Table({ exchangeData }) {
-  const { CAD, CHF, EUR, IDR, JPY, USD } = exchangeData;
-
+  const currencyList = ["CAD", "CHF", "EUR", "IDR", "JPY", "USD"];
   return (
     <>
       <table>
-        <tr id="head-table">
-          <th></th>
-          <th>WE BUY</th>
-          <th>EXCHANGE RATE</th>
-          <th>WE SELL</th>
-        </tr>
-        <tr className="content">
-          <td>CAD</td>
-          <td>{toBuy(CAD)}</td>
-          <td>{CAD}</td>
-          <td>{toSell(CAD)}</td>
-        </tr>
-        <tr className="content">
-          <td>IDR</td>
-          <td>{toBuy(IDR)}</td>
-          <td>{IDR}</td>
-          <td>{toSell(IDR)}</td>
-        </tr>
-        <tr className="content">
-          <td>JPY</td>
-          <td>{toBuy(JPY)}</td>
-          <td>{JPY}</td>
-          <td>{toSell(JPY)}</td>
-        </tr>
-        <tr className="content">
-          <td>CHF</td>
-          <td>{toBuy(CHF)}</td>
-          <td>{CHF}</td>
-          <td>{toSell(CHF)}</td>
-        </tr>
-        <tr className="content">
-          <td>EUR</td>
-          <td>{toBuy(EUR)}</td>
-          <td>{EUR}</td>
-          <td>{toSell(EUR)}</td>
-        </tr>
-        <tr className="content">
-          <td>USD</td>
-          <td>{toBuy(USD)}</td>
-          <td>{USD}</td>
-          <td>{toSell(USD)}</td>
-        </tr>
+        <thead>
+          <tr id="head-table">
+            <th></th>
+            <th>WE BUY</th>
+            <th>EXCHANGE RATE</th>
+            <th>WE SELL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currencyList.map((currency, index) => {
+            return (
+              <tr key={index} className="content">
+                <td>{currency}</td>
+                <td>{toBuy(exchangeData[currency])}</td>
+                <td>{exchangeData[currency]}</td>
+                <td>{toSell(exchangeData[currency])}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </>
   );
 }
 
 function toBuy(exchangeRates) {
-  const buy = exchangeRates * 1.02;
-  const roundFourPlace = buy.toFixed(4);
-  return Number(roundFourPlace);
+  return (exchangeRates * 1.02).toFixed(4);
 }
 
 function toSell(exchageRates) {
-  const sell = exchageRates * 0.98;
-  const roundFourPlace = sell.toFixed(4);
-  return Number(roundFourPlace);
-}
-
-function convertFourPlaces(exchageRates) {
-  const roundFourPlace = exchageRates.toFixed(4);
-  return Number(roundFourPlace);
+  return (exchageRates * 0.98).toFixed(4);
 }
